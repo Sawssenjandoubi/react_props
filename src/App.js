924324f0159ec92c6baeeb3c6401f4f93f94b25d
sawssen <sawssen.jandoubi10@gmail.com>
2022-06-14 23:36:38 +0200

    adding new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Welcome from '.';
+ const App = () => {
+ return (
+   <div>
+     <Welcome>Arya Stark</Welcome>
+   </div>
+ );
+};
+
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+  const Welcome = props => { 
+    <div>
+ <h1>Welcome {props.children}</h1>;
+ </div>
+};
+export default Welcome
